d60defc0a8a6bf516b5bbe638786f83b6fab30b0
Robert Nelson <robertcnelson@gmail.com>
2018-05-08 14:26:47 -0500

    u-boot fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/get_all.sh
+++ scripts/get_all.sh
@@ -30,56 +30,56 @@ dl_web
 #file="debian-9.2-iot-armhf-2017-11-08.tar.xz"
 #dl_web
 
-if [ -f ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/gcc-linaro-6.4.1-2017.08-linux-manifest.txt ] ; then
-	rm -rf ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/ || true
-	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
-	cd ./toolchain/
-	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
-	cd ../
-else
-	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
-	cd ./toolchain/
-	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
-	cd ../
-fi
+#if [ -f ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/gcc-linaro-6.4.1-2017.08-linux-manifest.txt ] ; then
+#	rm -rf ./toolchain/gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf/ || true
+#	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
+#	cd ./toolchain/
+#	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+#	cd ../
+#else
+#	echo "extracting: gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz"
+#	cd ./toolchain/
+#	tar xf gcc-linaro-6.4.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+#	cd ../
+#fi
 
-if [ -f ./u-boot/u-boot-2017.11-rc4/Makefile ] ; then
-	rm -rf ./u-boot/u-boot-2017.11-rc4/ || true
-	echo "extracting: u-boot-2017.11-rc4.tar.bz2"
+if [ -f ./u-boot/u-boot-2018.03/Makefile ] ; then
+	rm -rf ./u-boot/u-boot-2018.03/ || true
+	echo "extracting: u-boot-2018.03.tar.bz2"
 	cd ./u-boot/
-	tar xf u-boot-2017.11-rc4.tar.bz2
+	tar xf u-boot-2018.03.tar.bz2
 	cd ../
 else
-	echo "extracting: u-boot-2017.11-rc4.tar.bz2"
+	echo "extracting: u-boot-2018.03.tar.bz2"
 	cd ./u-boot/
-	tar xf u-boot-2017.11-rc4.tar.bz2
+	tar xf u-boot-2018.03.tar.bz2
 	cd ../
 fi
 
-if [ -f ./linux/linux-4.14/Makefile ] ; then
-	rm -rf ./linux/linux-4.14/ || true
-	echo "extracting: linux-4.14.tar.xz"
-	cd ./linux/
-	tar xf linux-4.14.tar.xz
-	cd ../
-else
-	echo "extracting: linux-4.14.tar.xz"
-	cd ./linux/
-	tar xf linux-4.14.tar.xz
-	cd ../
-fi
+#if [ -f ./linux/linux-4.14/Makefile ] ; then
+#	rm -rf ./linux/linux-4.14/ || true
+#	echo "extracting: linux-4.14.tar.xz"
+#	cd ./linux/
+#	tar xf linux-4.14.tar.xz
+#	cd ../
+#else
+#	echo "extracting: linux-4.14.tar.xz"
+#	cd ./linux/
+#	tar xf linux-4.14.tar.xz
+#	cd ../
+#fi
 
-if [ -f ./rootfs/debian-9.2-iot-armhf-2017-11-08/armhf-rootfs-debian-stretch.tar ] ; then
-	rm -rf ./rootfs/debian-9.2-iot-armhf-2017-11-08/ || true
-	echo "extracting: debian-9.2-iot-armhf-2017-11-08.tar.xz"
-	cd ./rootfs/
-	tar xf debian-9.2-iot-armhf-2017-11-08.tar.xz
-	cd ../
-else
-	echo "extracting: debian-9.2-iot-armhf-2017-11-08.tar.xz"
-	cd ./rootfs/
-	tar xf debian-9.2-iot-armhf-2017-11-08.tar.xz
-	cd ../
-fi
+#if [ -f ./rootfs/debian-9.2-iot-armhf-2017-11-08/armhf-rootfs-debian-stretch.tar ] ; then
+#	rm -rf ./rootfs/debian-9.2-iot-armhf-2017-11-08/ || true
+#	echo "extracting: debian-9.2-iot-armhf-2017-11-08.tar.xz"
+#	cd ./rootfs/
+#	tar xf debian-9.2-iot-armhf-2017-11-08.tar.xz
+#	cd ../
+#else
+#	echo "extracting: debian-9.2-iot-armhf-2017-11-08.tar.xz"
+#	cd ./rootfs/
+#	tar xf debian-9.2-iot-armhf-2017-11-08.tar.xz
+#	cd ../
+#fi
 
 
