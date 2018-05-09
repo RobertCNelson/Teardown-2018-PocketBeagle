116ca26e385f25207037628ca8563bced3fa9292
Robert Nelson <robertcnelson@gmail.com>
2018-05-09 14:41:29 -0500

    add pwm wait
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- examples/05_gpio_pwm.sh
+++ examples/05_gpio_pwm.sh
@@ -4,6 +4,10 @@ cd /sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip*/
 
 echo "0 > export"
 echo 0 > export
+
+#Waiting for udev to add permissions
+sleep 1
+
 echo "20000 -> period"
 echo 20000 > pwm-2\:0/period
 echo "10000: -> duty_cycle"
