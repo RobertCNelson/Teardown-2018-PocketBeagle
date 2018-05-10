7aa66f7d8e54fca6d27c652f7e22b9f1c71aed24
Robert Nelson <robertcnelson@gmail.com>
2018-05-10 09:35:28 -0500

    use p2_01 for everything
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- examples/05_gpio_setup.sh
+++ examples/05_gpio_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "GPIO59: config-pin -l P2_01"
+echo "config-pin -l P2_01"
 config-pin -l P2_01
 sleep 1
 
-echo "GPIO59: config-pin -q P2_01"
+echo "config-pin -q P2_01"
 config-pin -q P2_01
 sleep 1
 
-echo "GPIO59: config-pin P2_01 gpio"
+echo "config-pin P2_01 gpio"
 config-pin P2_01 gpio
 sleep 1
